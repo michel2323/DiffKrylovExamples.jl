27e85e740de41e914df93db522d3bc997a3a98a7
Michel Schanen <mschanen@anl.gov>
2024-05-08 15:52:51 -0500

    Fixing test for GMRES

--- test/test_adjoint_ilu_gmres.jl
+++ test/test_adjoint_ilu_gmres.jl
@@ -8,13 +8,34 @@ using Test
 include("../src/scaling.jl")
 include("../src/load_case.jl")
 
-# scaling callback
+# Stopping condition for GMRES against norm(b-Ax)
 function custom_stopping_condition(solver::KrylovSolver, A, b, r, tol)
-    mul!(r, A, solver.x)
+    z = solver.z
+    k = solver.inner_iter
+    nr = sum(1:k)
+    V = solver.V
+    R = solver.R
+    y = copy(z)
+
+    # Solve Rk * yk = zk
+    for i = k : -1 : 1
+        pos = nr + i - k
+        for j = k : -1 : i+1
+        y[i] = y[i] - R[pos] * y[j]
+        pos = pos - j + 1
+        end
+        y[i] = y[i] / R[pos]
+    end
+
+    # xk = Vk * yk
+    xk = sum(V[i] * y[i] for i = 1:k)
+    mul!(r, A, xk)
     r .-= b               # r := b - Ax
     bool = norm(r) ≤ tol  # tolerance based on the 2-norm of the residual
     return bool
 end
+
+
 A, _ = load_case("cdde4")
 b = ones(size(A,1))
 r = similar(b)
@@ -22,7 +43,7 @@ r = similar(b)
 dx = deepcopy(b)
 tb = deepcopy(b)
 # Create tranpose system to check against
-tA = sparse(transpose(A))
+tA = sparse(transpose(deepcopy(A)))
 
 # Apply scaling
 A, Dr, Dc = unsymmetric_scaling(A)
@@ -41,7 +62,7 @@ tsolver = GmresSolver(tA, tb)
 adjsolver = GmresSolver(A, dx)
 
 # Set up callbacks
-tol = 1e-6
+tol = 1e-10
 krylov_callback(_solver) = custom_stopping_condition(_solver, A, b, r, tol)
 tkrylov_callback(_solver) = custom_stopping_condition(_solver, tA, tb, r, tol)
 adjkrylov_callback(_solver) = custom_stopping_condition(_solver, adjoint(A), dx, r, tol)
@@ -52,14 +73,13 @@ rtol = eps(Float64)
 
 # Solve
 gmres!(solver, A, b; M=iluA, N=I, verbose=0, callback=krylov_callback, ldiv=true, atol=atol, rtol=rtol)
-gmres!(tsolver, tA, tb; M=ilutA, N=I, verbose=1, callback=tkrylov_callback, ldiv=true, atol=atol, rtol=rtol)
-gmres!(adjsolver, adjoint(A), dx; M=adjoint(iluA), N=I, verbose=1, callback=adjkrylov_callback, ldiv=true, atol=atol, rtol=rtol)
+gmres!(tsolver, tA, tb; M=ilutA, N=I, verbose=0, callback=tkrylov_callback, ldiv=true, atol=atol, rtol=rtol)
+gmres!(adjsolver, adjoint(A), dx; M=adjoint(iluA), N=I, verbose=0, callback=adjkrylov_callback, ldiv=true, atol=atol, rtol=rtol)
 
 # Should be the same
 @test tsolver.stats.niter == adjsolver.stats.niter
+@test isapprox(tsolver.x, adjsolver.x)
 
 println("solve(A) iterations: $(solver.stats.niter)")
 println("solve(tA) iterations: $(tsolver.stats.niter)")
 println("solve(adjoint(A)) iterations: $(adjsolver.stats.niter)")
-# Transpose A for testing against adjoint(A)
-tA = sparse(transpose(A))

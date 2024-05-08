c97a5f18c78a49e237023f27648efb104930a081
Michel Schanen <mschanen@anl.gov>
2024-05-08 15:01:24 -0500

    Update test

--- test/test_adjoint_ilu_gmres.jl
+++ test/test_adjoint_ilu_gmres.jl
@@ -15,31 +15,35 @@ function custom_stopping_condition(solver::KrylovSolver, A, b, r, tol)
     bool = norm(r) ≤ tol  # tolerance based on the 2-norm of the residual
     return bool
 end
-A, b = load_case("cdde4")
+A, _ = load_case("cdde4")
+b = ones(size(A,1))
 r = similar(b)
-b = ones(length(b))
+# Use two separate RHS for adjoint(A) and transpose(A) solve
+dx = deepcopy(b)
+tb = deepcopy(b)
+# Create tranpose system to check against
+tA = sparse(transpose(A))
 
 # Apply scaling
 A, Dr, Dc = unsymmetric_scaling(A)
+tA, tDr, tDc = unsymmetric_scaling(tA)
+# Scale all RHS
 b = Dr*b
-# Use two separate RHS for adjoint(A) and transpose(A) solve
-dx = copy(b)
-bt = copy(b)
-# Create tranpose system to check against
-tA = sparse(transpose(A))
+dx = Dr*dx
+tb = tDr*tb
 # Create preconditioner
 iluA = ilu(A)
 ilutA = ilu(tA)
 
 # Create solvers
 solver = GmresSolver(A, b)
-adjsolver = GmresSolver(A, b)
 tsolver = GmresSolver(tA, tb)
+adjsolver = GmresSolver(A, dx)
 
 # Set up callbacks
 tol = 1e-6
 krylov_callback(_solver) = custom_stopping_condition(_solver, A, b, r, tol)
-tkrylov_callback(_solver) = custom_stopping_condition(_solver, tA, bt, r, tol)
+tkrylov_callback(_solver) = custom_stopping_condition(_solver, tA, tb, r, tol)
 adjkrylov_callback(_solver) = custom_stopping_condition(_solver, adjoint(A), dx, r, tol)
 
 # atol and rtol set to max

7d21c0a343a23313a180e1b9b0e22499fbd0c3e3
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2024-05-08 12:41:47 -0400

    Update scaling.jl

--- src/scaling.jl
+++ src/scaling.jl
@@ -1,8 +1,6 @@
-function scaleSID(A::SparseMatrixCSC, iprint::Bool=false)
+function symmetric_scaling(A::SparseMatrixCSC)
     n, m = size(A)
-    if iprint
-        println("\n scaleSID: Symmetric scaling of SID matrix.   n = $n m = $m  nnz = $(nnz(A))")
-    end
+    nnzA = nnz(A)
 
     scale = maximum(abs.(A), dims=1)[:]
     Iz = findall(scale .== 0)
@@ -17,19 +15,42 @@ function scaleSID(A::SparseMatrixCSC, iprint::Bool=false)
     d .= scale .* d        # dense diagonal of DAD
     scale .= sqrt.(scale)  # dense elements of D
     D = Diagonal(scale)    # sparse diagonal D
-    A2 = copy(A)
-    DAD = copy(A)
-    mul!(A2, A, D)
-    mul!(DAD, D, A2)
-
-    if iprint
-        jmin = argmin(scale)
-        jmax = argmax(scale)
-        smin = scale[jmin]
-        smax = scale[jmax]
-        println("\n\n  Min scale                     Max scale")
-        println("  Col $jmin $smin    Col $jmax $smax")
-    end
+    AL = tril(A, -1)
+    AL2 = copy(AL)
+    mul!(AL2, AL, D)
+    mul!(AL, D, AL2)
+    DAD = AL + AL' + spdiagm(0 => d)
 
     return DAD, D
+end
+
+function unsymmetric_scaling(A::SparseMatrixCSC)
+    n, m = size(A)
+    nnzA = nnz(A)
+
+    absA = abs.(A)
+    scale_row = maximum(absA, dims=1)[:]
+    scale_column = maximum(absA, dims=2)[:]
+
+    Ir = findall(scale_row .== 0)
+    if length(Ir) > 0
+        scale_row[Ir] .= 1
+    end
+    Ic = findall(scale_column .== 0)
+    if length(Ic) > 0
+        scale_column[Ic] .= 1
+    end 
+
+    scale_row .= 1.0 ./ scale_row
+    scale_row .= sqrt.(scale_row)
+    Dr = Diagonal(scale_row)
+    scale_column .= 1.0 ./ scale_column
+    scale_column .= sqrt.(scale_column)
+    Dc = Diagonal(scale_column)
+    A2 = copy(A)
+    DAD = copy(A)
+    mul!(A2, A, Dc)
+    mul!(DAD, Dr, A2)
+
+    return DAD, Dr, Dc
 end
